53f9676b4a1d7ddb11c5f06cd8a63186b58639bd
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-01-23 16:00:30 -0500

    drivetrain stuff

--- src/org/usfirst/frc/team5254/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5254/robot/subsystems/Drivetrain.java
@@ -11,11 +11,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Drivetrain extends Subsystem {
 	
 	public static Spark driveControllerLeft1 = new Spark(RobotMap.DRIVETRAIN_LEFT);
-	public static Spark driveControllerLeft2 = new Spark(RobotMap.DRIVETRAIN_LEFT);
+	public static Spark driveControllerLeft2 = new Spark(RobotMap.DRIVETRAIN_LEFT2);
 	public static SpeedControllerGroup driveControllersLeft = new SpeedControllerGroup(driveControllerLeft1, driveControllerLeft2);
 	 
     public static Victor driveControllerRight1 = new Victor(RobotMap.DRIVETRAIN_RIGHT);
-    public static Victor driveControllerRight2 = new Victor(RobotMap.DRIVETRAIN_RIGHT);
+    public static Victor driveControllerRight2 = new Victor(RobotMap.DRIVETRAIN_RIGHT2);
     public static SpeedControllerGroup driveControllersRight = new SpeedControllerGroup(driveControllerRight1, driveControllerRight2);
     
     public static DifferentialDrive drivetrain = new
