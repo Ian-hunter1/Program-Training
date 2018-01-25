6f3587d5bc26ce55e6832b2c01638e798993ffc2
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-01-25 17:56:11 -0500

    commented

--- src/org/usfirst/frc/team5254/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team5254/robot/subsystems/Drivetrain.java
@@ -15,17 +15,23 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  */
 public class Drivetrain extends Subsystem {
 	
+	//spark encoders
 	public static Spark driveControllerLeft1 = new Spark(RobotMap.DRIVETRAIN_LEFT);
 	public static Spark driveControllerLeft2 = new Spark(RobotMap.DRIVETRAIN_LEFT2);
 	public static SpeedControllerGroup driveControllersLeft = new SpeedControllerGroup(driveControllerLeft1, driveControllerLeft2);
 	 
+	//victor encoders
     public static Victor driveControllerRight1 = new Victor(RobotMap.DRIVETRAIN_RIGHT);
     public static Victor driveControllerRight2 = new Victor(RobotMap.DRIVETRAIN_RIGHT2);
     public static SpeedControllerGroup driveControllersRight = new SpeedControllerGroup(driveControllerRight1, driveControllerRight2);
     
+    //differential drive
     public static DifferentialDrive drivetrain = new DifferentialDrive(driveControllersLeft, driveControllersRight);
+    
+    //pistons
     public static Solenoid shiftingPiston = new Solenoid(RobotMap.SHIFTING_PISTON);
     
+    //calling commands
     public Drivetrain() {     
     }
     public void stop() {
