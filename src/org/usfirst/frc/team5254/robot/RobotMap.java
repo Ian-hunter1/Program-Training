2e61b7de0be2640a55491e55dcce4b46608ab8fd
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-01-26 17:39:03 -0500

    cube mech + robot Map

--- src/org/usfirst/frc/team5254/robot/RobotMap.java
+++ src/org/usfirst/frc/team5254/robot/RobotMap.java
@@ -15,7 +15,8 @@ public class RobotMap {
 	public static final int DRIVETRAIN_LEFT2 = 1;
 	public static final int	DRIVETRAIN_RIGHT = 2;
 	public static final int DRIVETRAIN_RIGHT2 = 3;
-	public static final int CUBE_MECH_LEFT = 
+	public static final int CUBE_MECH_LEFT = 4;
+	public static final int CUBE_MECH_RIGHT = 5;
 	
 	//Joysticks
 	public static final int DRIVER_JOYSTICK = 0;
