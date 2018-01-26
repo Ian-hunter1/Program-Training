dfe610f5ae2e9796be29d966d1559ee786a29d05
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-01-26 18:42:23 -0500

    FINISHED CUBE MECH SUBSYSTEM!!!!!!!!!!!!!!!

--- src/org/usfirst/frc/team5254/robot/subsystems/CubeMech.java
+++ src/org/usfirst/frc/team5254/robot/subsystems/CubeMech.java
@@ -4,14 +4,40 @@ import org.usfirst.frc.team5254.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class CubeMech extends Subsystem {
 	
+	public static VictorSP cubeMechLeftFlywheels = new VictorSP(RobotMap.CUBE_MECH_LEFT);
+	public static VictorSP cubeMechRightFlywheels = new VictorSP(RobotMap.CUBE_MECH_RIGHT);
 	
+	public static Solenoid cubeMechArms = new Solenoid(RobotMap.CUBE_MECH_ARMS);
+	public static DoubleSolenoid cubeMechHinge = new DoubleSolenoid(RobotMap.CUBE_MECH_HINGE_UP, RobotMap.CUBE_MECH_HINGE_DOWN);
 	
+	public CubeMech () {
+	}
+	
+	public void ArmsDown() {
+		cubeMechHinge.set(DoubleSolenoid.Value.kReverse);
+	}
+	public void ArmsUp() {
+		cubeMechHinge.set(DoubleSolenoid.Value.kForward);
+	}
+	public void Intake() {
+		cubeMechLeftFlywheels.set(1);
+		cubeMechRightFlywheels.set(1);
+	}
+	public void Outake() {
+		cubeMechLeftFlywheels.set(-1);
+		cubeMechRightFlywheels.set(-1);
+	}
+	public void StopFlywheels() {
+		cubeMechLeftFlywheels.set(0);
+		cubeMechRightFlywheels.set(0);
+	}
     public void initDefaultCommand() {
     	
-    	public static VictorSP cubeMechLeftFlywheels = new VictorSP(RobotMap.CUBE_MECH_LEFT);
     	
     }
 }
