4f4b6f4382f421392bd35e654690868ba055b58d
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-02-01 18:06:57 -0500

    made life easier for myself

--- src/org/usfirst/frc/team5254/robot/autos/CrossAutoLine.java
+++ src/org/usfirst/frc/team5254/robot/autos/CrossAutoLine.java
@@ -1,28 +1,17 @@
 package org.usfirst.frc.team5254.robot.autos;
 
+import org.usfirst.frc.team5254.robot.Robot;
+import org.usfirst.frc.team5254.robot.autocommands.DriveToDistance;
+import org.usfirst.frc.team5254.robot.autocommands.LowerArms;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
 public class CrossAutoLine extends CommandGroup {
 
     public CrossAutoLine() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+        requires(Robot.Drivetrain);
+        requires(Robot.CubeMech);
+        
+        addSequential(new LowerArms());
+        addParallel(new DriveToDistance(1.0, 108));
     }
 }
--- src/org/usfirst/frc/team5254/robot/autos/Nothing.java
+++ src/org/usfirst/frc/team5254/robot/autos/Nothing.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team5254.robot.autos;
 
 import org.usfirst.frc.team5254.robot.Robot;
-import org.usfirst.frc.team5254.robot.commands.CubeMechArmsDown;
+import org.usfirst.frc.team5254.robot.autocommands.LowerArms;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class Nothing extends CommandGroup {
@@ -9,6 +9,6 @@ public class Nothing extends CommandGroup {
     public Nothing() {
         requires(Robot.CubeMech);
         
-        addSequential(new CubeMechArmsDown());
+        addSequential(new LowerArms());
     }
 }
