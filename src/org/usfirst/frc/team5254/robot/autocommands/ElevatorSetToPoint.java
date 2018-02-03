61f4cc7cd763c3a54f20c972cc5ec106ab7ccbb9
Ian Hunter <ianhunter@students.tburg.k12.ny.us>
2018-02-03 13:50:43 -0500

    dunn done dunzo all done yup its done it's all done you betcha it's done

--- src/org/usfirst/frc/team5254/robot/autocommands/ElevatorSetToPoint.java
+++ src/org/usfirst/frc/team5254/robot/autocommands/ElevatorSetToPoint.java
@@ -1,12 +1,8 @@
 package org.usfirst.frc.team5254.robot.autocommands;
 
 import org.usfirst.frc.team5254.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class ElevatorSetToPoint extends Command {
 	
 	double Speed;
@@ -19,24 +15,27 @@ public class ElevatorSetToPoint extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
+    	Robot.Elevator.autoTimedRaiseInit(Speed, Distance);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.Elevator.autoRaiseDistance(Speed, Distance);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.Elevator.raiseAutoIsFinished();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.Elevator.ElevatorStop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- src/org/usfirst/frc/team5254/robot/autos/PlaceOnSwitchLeft.java
+++ src/org/usfirst/frc/team5254/robot/autos/PlaceOnSwitchLeft.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team5254.robot.autos;
 
+import org.usfirst.frc.team5254.robot.Robot;
+import org.usfirst.frc.team5254.robot.autocommands.DriveToDistance;
+import org.usfirst.frc.team5254.robot.autocommands.ElevatorSetToPoint;
+import org.usfirst.frc.team5254.robot.autocommands.LowerArms;
+import org.usfirst.frc.team5254.robot.autocommands.PIDTurn;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -7,22 +14,25 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class PlaceOnSwitchLeft extends CommandGroup {
 
+	private static Timer timer = new Timer();
+	
     public PlaceOnSwitchLeft() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    	timer.reset();
+    	timer.start();
+    	
+    	addSequential(new LowerArms());
+    	addParallel(new ElevatorSetToPoint(1.0, 0.8));
+    	addSequential(new DriveToDistance(1.0, 17));
+    	addSequential(new PIDTurn(-25));
+    	addSequential(new DriveToDistance(1.0, 80));
+    	addSequential(new PIDTurn(25));
+    	addSequential(new DriveToDistance(1.0, 17));
+    	
+    	if(timer.get() >= 10) {
+    		Robot.CubeMech.Outake();
+    	} 
+    	if(timer.get() >= 13) {
+    		Robot.CubeMech.StopFlywheels();
+    	}
     }
 }
